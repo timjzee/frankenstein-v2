e6ce938761bb4096284227dab32ac595777bb10e
timjzee <timjzee@gmail.com>
2018-04-07 14:42:50 +0200

    Initial classification and evaluation

---
+++ analysis/rollingStylo.R
@@ -0,0 +1,93 @@
+library(stylo)
+library(rjson)
+library(zoo)
+
+setwd("/Users/tim/GitHub/frankenstein-v2/analysis")
+
+# write(toJSON(tokenized.test.corpus$S_text.txt), "frankenstein_tokenized_r.json")
+
+tokenized.test.corpus = load.corpus.and.parse(corpus.dir = "./test_set",
+                                              encoding = "UTF-8",
+                                              splitting.rule = "[ ]+")
+summary(tokenized.test.corpus)
+
+tokenized.training.corpus = load.corpus.and.parse(corpus.dir = "./reference_set",
+                                                  encoding = "UTF-8")
+
+sample_size = 1500
+sample_overlap = 1400
+resolution = sample_size - sample_overlap
+
+sliced.test.corpus = make.samples(tokenized.test.corpus, sampling = "normal.sampling",
+                              sample.size = sample_size, sample.overlap = sample_overlap)
+
+sliced.training.corpus = make.samples(tokenized.training.corpus, sampling = "normal.sampling",
+                                      sample.size = sample_size, sample.overlap = 0)
+
+results = rolling.classify(test.corpus = sliced.test.corpus,
+                           training.corpus = sliced.training.corpus,
+                           write.png.file = TRUE, 
+                           classification.method = "svm", mfw = 100)
+
+svm_classification = as.vector(results$classification.results)
+
+hand_tokens = fromJSON(file = "/Users/tim/GitHub/frankenstein-v2/sga-data/output/hand_list_processed.json")
+num_samples = as.integer((length(hand_tokens) - sample_overlap) / resolution)
+num_words = (num_samples - 1) * resolution + sample_size
+culled_hand_tokens = hand_tokens[1:num_words]
+hand_matrix = rollapply(culled_hand_tokens, sample_size, by = resolution, c)
+hand_groups = split(hand_matrix, row(hand_matrix))
+hand_majorities = sapply(hand_groups, function(x) names(which.max(table(x))))
+
+summary(svm_classification)
+summary(hand_majorities)
+
+confusion_matrix = as.matrix(table(Handwriting = hand_majorities, Predicted = svm_classification))
+confusion_matrix
+
+num_classes = nrow(confusion_matrix)
+diag = diag(confusion_matrix)
+rowsums = apply(confusion_matrix, 1, sum)
+colsums = apply(confusion_matrix, 2, sum)
+p = rowsums / num_samples
+q = colsums / num_samples
+accuracy = sum(diag) / num_samples
+accuracy
+
+precision = diag / colsums 
+recall = diag / rowsums 
+f1 = 2 * precision * recall / (precision + recall)
+data.frame(precision, recall, f1)
+
+macroPrecision = mean(precision)
+macroRecall = mean(recall)
+macroF1 = mean(f1)
+data.frame(macroPrecision, macroRecall, macroF1)
+
+### baseline for comparison ###
+
+baseline_classification = rep("mws", num_samples)
+
+confusion_matrix = as.matrix(table(Handwriting = hand_majorities, Predicted = baseline_classification))
+confusion_matrix = cbind(confusion_matrix, pbs = c(0, 0))
+confusion_matrix
+
+num_classes = nrow(confusion_matrix)
+diag = diag(confusion_matrix)
+rowsums = apply(confusion_matrix, 1, sum)
+colsums = apply(confusion_matrix, 2, sum)
+p = rowsums / num_samples
+q = colsums / num_samples
+accuracy = sum(diag) / num_samples
+accuracy
+
+precision = c(diag["mws"] / colsums["mws"], 1)
+names(precision) = c("mws", "pbs")
+recall = diag / rowsums 
+f1 = 2 * precision * recall / (precision + recall)
+data.frame(precision, recall, f1)
+
+macroPrecision = mean(precision)
+macroRecall = mean(recall)
+macroF1 = mean(f1)
+data.frame(macroPrecision, macroRecall, macroF1)
---
+++ sga-data/postProcessing.py
@@ -0,0 +1,99 @@
+import json
+import re
+
+with open("./output/text.txt") as f:
+    text = f.read()
+
+with open("./output/text_list.json") as f:
+    text_list = json.loads(f.read())
+
+with open("./output/hand_list.json") as f:
+    hand_list = json.loads(f.read())
+
+# splitting regex
+
+# split_regex = "".join(["[^A-Za-z", "\U00C0-\U00FF", "\U0100-\U01BF", "\U01C4-\U02AF", "\U0386\U0388-\U03FF", "\U0400-\U0481\U048A-\U0527", "\U05D0-\U05EA\U05F0-\U05F4", "\U0620-\U065F\U066E-\U06D3\U06D5\U06DC", "\U1E00-\U1EFF", "\U1F00-\U1FBC\U1FC2-\U1FCC\U1FD0-\U1FDB\U1FE0-\U1FEC\U1FF2-\U1FFC", "\U03E2-\U03EF\U2C80-\U2CF3", "\U10A0-\U10FF", "]+"])
+
+# change within word hand changes
+
+counter = 0
+complete = True
+incomplete_word = None
+mws_score = 0
+pbs_score = 0
+new_text_list = []
+new_hand_list = []
+for segment in text_list:
+    hand = hand_list[counter]
+    # handle all completed words in a fragment
+    if complete:
+        word_list = re.findall(r'[^ ]+(?= )', segment)
+        new_text_list.extend(word_list)
+        new_hand_list.extend([hand] * len(word_list))
+    else:
+        if segment[0] == " ":
+            new_text_list.append(incomplete_word)
+            majority_hand = "mws" if mws_score > pbs_score else "pbs"
+            new_hand_list.append(majority_hand)
+            print(incomplete_word, "mws:", mws_score, "pbs:", pbs_score, "hand:", majority_hand)
+            incomplete_word = None
+            mws_score = 0
+            pbs_score = 0
+            complete = True
+            word_list = re.findall(r'[^ ]+(?= )', segment)
+            new_text_list.extend(word_list)
+            hand = hand_list[counter]
+            new_hand_list.extend([hand] * len(word_list))
+        else:  # i.e. if incomplete and start of segment is part of a word
+            word_part = re.match(r'^[^ ]+', segment).group()
+            incomplete_word += word_part[:]
+            if hand == "mws":
+                mws_score += len(word_part)
+            elif hand == "pbs":
+                pbs_score += len(word_part)
+#            segment = re.sub(r'^[^ ]+', '', segment)
+            word_list = re.findall(r'[^ ]+(?= )', segment[len(word_part):])
+            if len(word_list) == 0 and segment[-1] != " ":
+                if " " in segment:  # i.e. if one space in segment
+                    new_text_list.append(incomplete_word)
+                    majority_hand = "mws" if mws_score > pbs_score else "pbs"
+                    new_hand_list.append(majority_hand)
+                    print(incomplete_word, "mws:", mws_score, "pbs:", pbs_score, "hand:", majority_hand)
+                    incomplete_word = None
+                    mws_score = 0
+                    pbs_score = 0
+                    complete = True
+            else:
+                new_text_list.append(incomplete_word)
+                majority_hand = "mws" if mws_score > pbs_score else "pbs"
+                new_hand_list.append(majority_hand)
+                print(incomplete_word, "mws:", mws_score, "pbs:", pbs_score, "hand:", majority_hand)
+                incomplete_word = None
+                mws_score = 0
+                pbs_score = 0
+                new_text_list.extend(word_list)
+                new_hand_list.extend([hand] * len(word_list))
+                complete = True
+    # handle start of incompleted words
+    if segment[-1] != " ":
+        complete = False
+        word_start = re.search(r"[^ ]+$", segment).group()
+        if hand == "mws":
+            mws_score += len(word_start)
+        elif hand == "pbs":
+            pbs_score += len(word_start)
+        incomplete_word = word_start[:]
+    counter += 1
+
+if incomplete_word:
+    new_text_list.append(incomplete_word)
+    majority_hand = "mws" if mws_score > pbs_score else "pbs"
+    new_hand_list.append(majority_hand)
+    print(incomplete_word, "mws:", mws_score, "pbs:", pbs_score, "hand:", majority_hand)
+
+
+with open("./output/text_list_processed.json", "w") as f:
+    f.write(json.dumps(new_text_list))
+
+with open("./output/hand_list_processed.json", "w") as f:
+    f.write(json.dumps(new_hand_list))
