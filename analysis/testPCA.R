6eb53dd876df418512c13760eda4ab77fb24dee4
timjzee <timjzee@gmail.com>
2018-10-14 04:11:16 +0200

    Fixed PCA tokenization

--- analysis/testPCA.R
+++ analysis/testPCA.R
@@ -7,7 +7,7 @@ raw.corpus <- load.corpus(files = "all", corpus.dir = "./pca_texts",
                           encoding = "UTF-8")
 
 tokenized.corpus <- txt.to.words.ext(raw.corpus, language = "English.all",
-                                     preserve.case = FALSE)
+                                     splitting.rule = "[!(;'?\n^).,>\":= \u2014\u2013]+")
 summary(tokenized.corpus)
 
 sliced.corpus <- make.samples(tokenized.corpus, sampling = "normal.sampling",
@@ -18,10 +18,11 @@ frequent.features <- fromJSON(file = "./f_words.json")
 
 freqs <- make.table.of.frequencies(sliced.corpus, features = frequent.features)
 
-stylo(frequencies = freqs, analysis.type = "PCR",
+pca.results = stylo(frequencies = freqs, analysis.type = "PCR",
       custom.graph.title = "Lamb vs. the Shelleys",
       mfw.min = 200, mfw.max = 200,
       pca.visual.flavour = "loadings",
       write.png.file = FALSE, gui = FALSE)
 
+summary(pca.results)
 par(mfrow = c(1, 1))
